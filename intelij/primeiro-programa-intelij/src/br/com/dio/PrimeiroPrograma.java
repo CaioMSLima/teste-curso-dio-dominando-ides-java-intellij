759e6444b0c808d6dd06226bb6e35524cf412ef8
caio <caiomslima@gmail.com>
2022-06-23 11:28:29 -0300

    commit 1

---
+++ intelij/primeiro-programa-intelij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Hiena;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Hiena hiena = new Hiena();
+
+        System.out.println(hiena);
+
+        int a = 5;
+        int b = 3;
+        System.out.println("Hello word! " + (a+b) );
+    }
+}
