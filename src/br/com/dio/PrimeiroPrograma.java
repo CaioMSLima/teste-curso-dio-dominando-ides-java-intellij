73896a177628163680b0d481c319f09837f2f86c
caio <caiomslima@gmail.com>
2022-06-23 12:04:45 -0300

    commit 2

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,8 +8,48 @@ public class PrimeiroPrograma {
 
         System.out.println(hiena);
 
+        Livros Livros = new Livros( nome: "o problema dos 3 corpos", numPagina: 300);
+
         int a = 5;
         int b = 3;
         System.out.println("Hello word! " + (a+b) );
     }
 }
+
+class Livros {
+    private String nome;
+    private Integer numPagina;
+
+    public Livros(String nome, Integer numPagina) {
+        this.nome = nome;
+        this.numPagina = numPagina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPagina() {
+        return numPagina;
+    }
+
+    public void setNumPagina(Integer numPagina) {
+        this.numPagina = numPagina;
+    }
+
+    @Override
+    public String toString() {
+        return "Livros{" +
+                "nome='" + nome + '\'' +
+                ", numPagina=" + numPagina +
+                '}';
+    }
+
+
+
+}
+
